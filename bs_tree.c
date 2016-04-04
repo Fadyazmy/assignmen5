b0e6751653723068007445668eb799aa3e65bb37
Fady Azmy <Fadyazmy@users.noreply.github.com>
2016-04-03 23:18:57 -0400

    update

--- bs_tree.c
+++ bs_tree.c
@@ -30,28 +30,29 @@ if((*bst) == NULL){ // if root is NULL
 
 else{
 //using method recursively bs_tree_insert_node
-bs_tree_insert_node(*(bst), key, data);
+bs_tree_insert_node(bst, key, data);
 	}//bs_tree_insert function
 }
+
 //helping
-void bs_tree_insert_node(struct Bst_Node *node, Key key, Data_Item data){
+void bs_tree_insert_node(struct Bst_Node **node, Key key, Data_Item data){
 
 	if(node == NULL){
 	//current node is NULL
-	node = (BStree_node*)malloc(sizeof(BStree_node));
-	node->key = key; //root.key 
-	node->data = data;//root.data 
-	node->left = (BStree_node*)malloc(sizeof(BStree_node));//root.key 
-	node->right = (BStree_node*)malloc(sizeof(BStree_node));//root.key 
+	(*node) = (BStree_node*)malloc(sizeof(BStree_node));
+	(*node)->key = key; //root.key 
+	(*node)->data = data;//root.data 
+	(*node)->left = (BStree_node*)malloc(sizeof(BStree_node));//root.key 
+	(*node)->right = (BStree_node*)malloc(sizeof(BStree_node));//root.key 
 	}
-	else if(key_comp(node->key, key) == 1){//go right
+	else if(key_comp((*node)->key, key) == 1){//go right
 
 		printf("insert r\n");
-	bs_tree_insert_node(node->right, key, data);
+	bs_tree_insert_node(&((*node)->right), key, data);
 	}//if function
-	else if(key_comp(node->key, key) == -1){ // go left
+	else if(key_comp((*node)->key, key) == -1){ // go left
 			printf("insert l\n");
-	bs_tree_insert_node(node->left, key, data);
+	bs_tree_insert_node(&((*node)->left), key, data);
 	}
 	else{
 			printf("node already exists\n"); //identical node already exist
@@ -75,7 +76,7 @@ else if(key_comp((**bst).key, key) == 0){
 return &(**bst).data;//identical data item node
 }
 else{ //must be NULL
- //current node is NULL
+//current node is NULL
 //Not found
 	printf("Node not found\n");
 	return NULL;
